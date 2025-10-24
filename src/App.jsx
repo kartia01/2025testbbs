761ea3d2782561908787b4c6d07a7f2e2f7f6721
kartia01 <kartia25142@gmail.com>
2025-10-24 15:51:57 +0900

    testsb

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import supabase from './utils/supabase';
 
 function App() {
-  return <div>App</div>;
-}
+  const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    async function getPosts() {
+      const { data: posts } = await supabase.from('posts').select();
+
+      setPosts(posts);
+
+      console.log(posts);
+    }
+
+    getPosts();
+  }, []);
+
+  return (
+    <div>
+      <h3>post</h3>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={i}>{post.TITLE}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 export default App;
