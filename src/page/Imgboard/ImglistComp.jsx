dd84f5fbefcec8c4929fa84ab7ab9b4f3d4b0cb5
kartia01 <kartia25142@gmail.com>
2025-11-14 01:18:11 +0900

    img

--- src/page/Imgboard/ImglistComp.jsx
+++ src/page/Imgboard/ImglistComp.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { useImg } from '../../components/context/ImgContext';
-import { Link } from 'react-router-dom';
-import dayjs from 'dayjs';
+import React, { useState } from "react";
+import { useImg } from "../../components/context/ImgContext";
+import { Link } from "react-router-dom";
+import dayjs from "dayjs";
 
 function ImglistComp() {
   const { iboard } = useImg();
@@ -10,24 +10,36 @@ function ImglistComp() {
   return (
     <div className="container mt-4">
       <h3>이미지 게시판</h3>
-      <div className="d-flex justify-content-center gap-3">
-        {iboard.map((item, i) => {
-          return (
-            <Link to={`/img/imgview/${item.id}`} className="nav-link border border-1" key={i}>
-              <ul>
-                <li>{item.fileurl}</li>
-                <li className="ms-1">{item.title}</li>
-                <li className="ms-1">{item.name}</li>
-                <li>{dayjs(item.created_at).format('YY-MM-DD')}</li>
-              </ul>
-            </Link>
-          );
-        })}
+      <div className="row">
+        {iboard && iboard.length > 0 ? (
+          iboard.map((item, i) => (
+            <div className="col-12 col-md-4" key={i}>
+              <Link
+                to={`/img/imgview/${item.id}`}
+                className="nav-link border border-1"
+              >
+                {item.fileurl ? (
+                  // || (논리 OR 연산자)는 **"왼쪽 값이 없으면 오른쪽 값을 대신 써라
+                  <img src={item.fileurl} alt={item.title || "image"} />
+                ) : (
+                  <div>no image</div>
+                )}
+                <ul>
+                  <li className="ms-1">{item.title}</li>
+                  <li className="ms-1">{item.name}</li>
+                  <li>{dayjs(item.created_at).format("YY-MM-DD")}</li>
+                </ul>
+              </Link>
+            </div>
+          ))
+        ) : (
+          <p>등록된 이미지가 없습니다.</p>
+        )}
       </div>
 
       <div className="d-flex justify-content-end">
         <div className="d-flex gap-2">
-          <Link to="/img/imgwrite" className="btn btn-primary">
+          <Link to="/img/imgwrite" className="btn btn-primary mt-3">
             글작성
           </Link>
         </div>
--- src/page/Imgboard/ImgviewComp.jsx
+++ src/page/Imgboard/ImgviewComp.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import supabase from '../../utils/supabase';
-import dayjs from 'dayjs';
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import supabase from "../../utils/supabase";
+import dayjs from "dayjs";
 
-function ImgviewComp(post) {
+function ImgviewComp() {
   const params = useParams();
   const { id } = useParams();
   console.log(params);
@@ -11,13 +11,16 @@ function ImgviewComp(post) {
 
   useEffect(() => {
     const imgData = async () => {
-      const { data, error } = await supabase.from('imgboard').select('*').eq('id', Number(id)).single();
+      const { data, error } = await supabase
+        .from("imgboard")
+        .select("*")
+        .eq("id", Number(id))
+        .single();
       console.log(data);
       setImg(data);
     };
     imgData();
   }, []);
-
 
   return (
     // 사용할 것 : img파일, title, name, content
@@ -25,10 +28,23 @@ function ImgviewComp(post) {
       <h3>이미지 보기</h3>
       <hr />
       <div>
-        <img src={post.uploadUrl} alt={post.fileName} />
+        {img.fileurl ? (
+          <img
+            src={img.fileurl}
+            alt={img.filename}
+            style={{
+              width: "300px",
+              height: "300px",
+              objectFit: "cover",
+              borderRadius: "10px",
+            }}
+          />
+        ) : (
+          <p>이미지를 불러올 수 없습니다.</p>
+        )}
         <h4>{img.title}</h4>
         <div>
-          {img.name} / {dayjs(img.created_at).format('YY.MM.DD hh:mm')}
+          {img.name} / {dayjs(img.created_at).format("YY.MM.DD hh:mm")}
         </div>
         <hr />
         <p>{img.content}</p>
